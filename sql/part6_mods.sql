29f818b6fb9c92c682c0f3f1f150212b3f9cb10a
System User <cs421g16@comp421.CS.McGill.CA>
2018-03-01 20:29:43 -0500

    remove unused scripts

--- sql/part6_mods.sql
+++
@@ -1,5 +0,0 @@
-
--- 1) Update the following_count number for each page:
-UPDATE page SET follower_count = (SELECT COUNT(*) FROM page_follower WHERE page_follower.page_handle = page.page_handle); 
--- 1) Print the results
-SELECT * FROM page LIMIT 50
